ed93509445d8bf6e7f551584722ea8adca8a0ca2
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-08 15:33:37 +0200

    initializing the component

--- app/src/main/java/ws/tilda/anastasia/dagger2project/activity/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package ws.tilda.anastasia.dagger2project.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import ws.tilda.anastasia.dagger2project.R;
+import ws.tilda.anastasia.dagger2project.activity.di.ActivityComponent;
+import ws.tilda.anastasia.dagger2project.activity.di.ActivityModule;
+import ws.tilda.anastasia.dagger2project.activity.di.DaggerActivityComponent;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityComponent component;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        component = DaggerActivityComponent.builder()
+                .activityModule(new ActivityModule())
+                .build();
     }
 }
