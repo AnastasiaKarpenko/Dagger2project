4eb857c6498655093906c7e7fbcde9e1c10e0ba2
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-08 15:30:47 +0200

    Creating ActivityModule and ActivityComponent. Stating the module in the component interface

---
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/activity/di/ActivityComponent.java
@@ -0,0 +1,7 @@
+package ws.tilda.anastasia.dagger2project.activity.di;
+
+import dagger.Component;
+
+@Component(modules = {ActivityModule.class})
+public interface ActivityComponent {
+}
---
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/activity/di/ActivityModule.java
@@ -0,0 +1,7 @@
+package ws.tilda.anastasia.dagger2project.activity.di;
+
+import dagger.Module;
+
+@Module
+public class ActivityModule {
+}
