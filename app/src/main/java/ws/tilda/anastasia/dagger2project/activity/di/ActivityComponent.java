fa7972b3dea2ba7d8459433b1bd88b45753b5794
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-08 15:35:01 +0200

    Adding dependencies in ActivityComponent to ApplicationComponent telling it to use it when needed

--- app/src/main/java/ws/tilda/anastasia/dagger2project/activity/di/ActivityComponent.java
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/activity/di/ActivityComponent.java
@@ -1,7 +1,8 @@
 package ws.tilda.anastasia.dagger2project.activity.di;
 
 import dagger.Component;
+import ws.tilda.anastasia.dagger2project.application.di.ApplicationComponent;
 
-@Component(modules = {ActivityModule.class})
+@Component(modules = {ActivityModule.class}, dependencies = {ApplicationComponent.class})
 public interface ActivityComponent {
 }
