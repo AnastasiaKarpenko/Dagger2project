d94fd6030b4126e26bb0c8945e86120f3cc333c2
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-08 15:28:14 +0200

    Created Dagger application component

--- app/src/main/java/ws/tilda/anastasia/dagger2project/application/MyApplication.java
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/application/MyApplication.java
@@ -2,6 +2,20 @@ package ws.tilda.anastasia.dagger2project.application;
 
 import android.app.Application;
 
+import ws.tilda.anastasia.dagger2project.application.di.ApplicationComponent;
+import ws.tilda.anastasia.dagger2project.application.di.ApplicationModule;
+import ws.tilda.anastasia.dagger2project.application.di.DaggerApplicationComponent;
+
 public class MyApplication extends Application {
 
+    ApplicationComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        component = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule())
+                .build();
+    }
 }
