f666dbc529377d552c38ee5a0c32edb92eca9eec
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-08 16:34:25 +0200

    Added BASE_URL in the Build Config field in the gradle app file. Provided retrofit in the application module as well as gsonConverterFactory method

--- app/src/main/java/ws/tilda/anastasia/dagger2project/application/di/ApplicationModule.java
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/application/di/ApplicationModule.java
@@ -1,7 +1,25 @@
 package ws.tilda.anastasia.dagger2project.application.di;
 
 import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import ws.tilda.anastasia.dagger2project.BuildConfig;
 
 @Module
 public class ApplicationModule {
+
+    @Provides
+    Retrofit retrofit(GsonConverterFactory gsonConverterFactory) {
+        return new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(gsonConverterFactory)
+                .build();
+
+    }
+
+    @Provides
+    GsonConverterFactory gsonConverterFactory() {
+        return null;
+    }
 }
