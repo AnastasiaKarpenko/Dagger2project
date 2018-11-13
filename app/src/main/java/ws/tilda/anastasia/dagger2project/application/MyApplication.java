a2d3d232ec84a22574595e6e2ec6d692b9a64a3e
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-13 14:49:27 +0200

    Initialized the call to the service

--- app/src/main/java/ws/tilda/anastasia/dagger2project/application/MyApplication.java
+++ app/src/main/java/ws/tilda/anastasia/dagger2project/application/MyApplication.java
@@ -1,14 +1,19 @@
 package ws.tilda.anastasia.dagger2project.application;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import ws.tilda.anastasia.dagger2project.BuildConfig;
 import ws.tilda.anastasia.dagger2project.model.CredentialsManager;
+import ws.tilda.anastasia.dagger2project.model.CredentialsObject;
 import ws.tilda.anastasia.dagger2project.model.RestService;
 
 public class MyApplication extends Application {
@@ -31,6 +36,24 @@ public class MyApplication extends Application {
                 .build();
         credentialsManager = new CredentialsManager();
         RestService restService = credentialsManager.getService(retrofit);
+
+        Call<CredentialsObject> call = restService.getCredentials();
+
+        call.enqueue(new Callback<CredentialsObject>() {
+            @Override
+            public void onResponse(Call<CredentialsObject> call, Response<CredentialsObject> response) {
+                CredentialsObject credentialsObject = response.body();
+                Log.d("response data", "onResponse: apiKey " + credentialsObject.getApiKey());
+                Log.d("response data", "onResponse: sessionId " + credentialsObject.getSessionId());
+                Log.d("response data", "onResponse: token " + credentialsObject.getToken());
+
+            }
+
+            @Override
+            public void onFailure(Call<CredentialsObject> call, Throwable t) {
+
+            }
+        });
 
     }
 }
